c743b7576177e8283342852c347d923030b27dc0
Camille Police <camillepolice@icloud.com>
2025-07-29 21:57:38 +0200

    chore: install linting tools + http client

---
+++ rector.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__ . '/var',
+        __DIR__ . '/vendor',
+    ]);
+
+    // Define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_83,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+    ]);
+};
